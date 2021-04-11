f44b1a92d194f96750a2a2f48d68a026f1f13595
Daniel Hess <dan9186@gmail.com>
2021-04-11 16:13:16 -0700

    reduce the dbblocker to use the callback blocker

--- dbblocker/dbblocker.go
+++ dbblocker/dbblocker.go
@@ -3,38 +3,11 @@ package dbblocker
 import (
 	"database/sql"
 	"time"
+
+	"github.com/gomicro/blockit/cbblocker"
 )
 
-// Blocker represents a SQL database connection to monitor for connectivity
-// and block until a connection is established.
-type Blocker struct {
-	db *sql.DB
-}
-
 // New takes a SQL database object and returns a newly instantiated Blocker
-func New(db *sql.DB) *Blocker {
-	return &Blocker{db: db}
-}
-
-// Blockit meets the blocker interface. It returns a read only channel that will
-// receive true when the database is connected.
-func (d *Blocker) Blockit() <-chan bool {
-	connected := make(chan bool)
-
-	go func() {
-		for {
-			err := d.db.Ping()
-			if err != nil {
-				<-time.After(1 * time.Second)
-				continue
-			}
-
-			connected <- true
-			close(connected)
-
-			break
-		}
-	}()
-
-	return connected
+func New(db *sql.DB) *cbblocker.Blocker {
+	return cbblocker.New(db.Ping, 1*time.Second)
 }
