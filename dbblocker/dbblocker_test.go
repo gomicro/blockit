4ca6cedbf35b2d757e286723c344d11538da0414
Daniel Hess <dan9186@gmail.com>
2021-04-11 16:02:09 -0700

    fix another test to be modern

--- dbblocker/dbblocker_test.go
+++ dbblocker/dbblocker_test.go
@@ -5,13 +5,13 @@ import (
 
 	"github.com/gomicro/blockit/dbblocker"
 
-	. "github.com/franela/goblin"
+	"github.com/franela/goblin"
 	. "github.com/onsi/gomega"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
 func TestDefaultBlockers(t *testing.T) {
-	g := Goblin(t)
+	g := goblin.Goblin(t)
 	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
 
 	g.Describe("DB Blocking", func() {
